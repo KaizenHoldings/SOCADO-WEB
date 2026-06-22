f6b474d96c56f29d1dd9867b517362323565eebf
KaizenHoldings <khtools@kaizenholdings.net>
2026-06-22 14:56:14 -0400

    [add] web - importMap

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -2,11 +2,13 @@ import { CsvActions as CsvActions_8c543b0fa55095ef663ee6361672f78b } from '@/com
 import { Icon as Icon_6f14b747d7d9d385f91171711bbf7132 } from '@/components/payload/Icon'
 import { Logo as Logo_e34c28bdc97e3fecf976b23b26d64cc9 } from '@/components/payload/Logo'
 import { CollectionCards as CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1 } from '@payloadcms/next/rsc'
+import { VercelBlobClientUploadHandler as VercelBlobClientUploadHandler_123 } from '@payloadcms/storage-vercel-blob/client'
 
 /** @type import('payload').ImportMap */
 export const importMap = {
   "@/components/admin/CsvActions#CsvActions": CsvActions_8c543b0fa55095ef663ee6361672f78b,
   "@/components/payload/Icon#Icon": Icon_6f14b747d7d9d385f91171711bbf7132,
   "@/components/payload/Logo#Logo": Logo_e34c28bdc97e3fecf976b23b26d64cc9,
-  "@payloadcms/next/rsc#CollectionCards": CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1
+  "@payloadcms/next/rsc#CollectionCards": CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1,
+  "@payloadcms/storage-vercel-blob/client#VercelBlobClientUploadHandler": VercelBlobClientUploadHandler_123
 }
